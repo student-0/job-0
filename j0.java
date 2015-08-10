e5026f18c4d00066ed1a7c285bec5db90311ddcc
Bruce Alan Martin <bam@suffolk.li>
2015-08-10 17:09:42 -0400

    Update j0.java

--- j0.java
+++ j0.java
@@ -4,13 +4,15 @@
 size( 640, 480 );
 background( 50, 100, 200 );
 
-// Body is a 30x50 rectangle, at coordinate (100,100).
+// Body is a 30x50 rectangle, centered at (100,100).
 fill(255,0,0);
+rectMode(CENTER);
 rect( 100,100, 30,50 );
+
 
 //  Modify the above code as follows:
 //    * change the color of the body.
 //    * add a head 
 //    * make it follow the mouse.
 
-//  When you are done, send me a "pull-request"
+//  When you are done, commit your code and send me a "pull-request"
