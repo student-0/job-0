d72467f41c0be10ffc97da3dd89671fbe4bd778e
Bruce Alan Martin <bam@suffolk.li>
2015-08-10 17:02:52 -0400

    Update j0.java

--- j0.java
+++ j0.java
@@ -1,6 +1,16 @@
 //// This is your starter code.
 //// Put your username here.
 
-fill(255,0,0);
-rect( mouseX,mouseY, 30,50 );
+size( 640, 480 );
+background( 50, 100, 200 );
 
+// Body is a 30x50 rectangle, at coordinate (100,100).
+fill(255,0,0);
+rect( 100,100, 30,50 );
+
+//  Modify the above code as follows:
+//    * change the color of the body.
+//    * add a head 
+//    * make it follow the mouse.
+
+//  When you are done, send me a "pull-request"
